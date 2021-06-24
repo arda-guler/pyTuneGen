84b9b877972a52465af39c3142a670047bc23765
orhun <orhun@archlinux.org>
2021-06-25 00:54:45 +0300

    Add web server for god to sing

--- godsong.py
+++ godsong.py
@@ -1,21 +1,8 @@
-from synthesizer import Player, Synthesizer, Waveform
+from flask import Flask, render_template
 import random
 import time
 import requests
-
-response = requests.get('https://godsays.xyz')
-
-summation = 0
-if response.status_code == 200:
-    godsaid = str(response.content)
-    for word in godsaid.split(" "):
-        for char in word:
-            summation += ord(char)
-else:
-    print("god is down")
-
-print("Seed:", summation)
-random.seed(summation)
+import os
 
 #-------------------------
 #  NOTE FREQUENCY TABLE
@@ -85,18 +72,24 @@ bpms = [bpm_slowest, bpm_slow, bpm_slowish, bpm_normal, bpm_fastish, bpm_fast, b
 note_durations = {"1/4":1/4, "1/2": 1/2, "1":1, "2":2, "4":4}
 
 #-------------------------
-#       SETUP
-#-------------------------
-player = Player()
-player.open_stream()
-synth1 = Synthesizer(osc1_waveform = Waveform.sine, osc1_volume=1.0,
-                     use_osc2=True, osc2_waveform=Waveform.square, osc2_volume=0.3)
-
-#-------------------------
-#       PLAY
+#       GENERATE
 #-------------------------
 
 def generateMusic():
+    response = requests.get('https://godsays.xyz')
+    summation = 0
+    godsaid = ""
+    if response.status_code == 200:
+        godsaid = str(response.content, 'utf-8')
+        for word in godsaid.split(" "):
+            for char in word:
+                summation += ord(char)
+    else:
+        print("god is down")
+
+    print("Seed:", summation)
+    random.seed(summation)
+
     # get random bpm, scale, chord, silence percent, length
     bpm_current = bpms[random.randint(0, len(bpms) - 1)]
     print("BPM: " + str(bpm_current))
@@ -115,15 +108,12 @@ def generateMusic():
 
     note_jump_limit = 600 # Hertz
 
-    silence_percent = 2
-
-    music_length = 50 # num. of bars
+    music_length = 10 # num. of bars
     print("Length:", music_length, "bars")
 
     music = []
 
     for i in range(0, music_length):
-
         # divide the next bar into notes of various lengths
         durations_current = []
 
@@ -134,21 +124,11 @@ def generateMusic():
             if sum(durations_current) + next_duration_value <= scale:
                 durations_current.append(next_duration_value)
 
-        #print("\n         Bar #" + str(i+1) + ": " + str(durations_current))
-
         note_last = None
         note_current_name = None
         note_current = None
 
         for duration in durations_current:
-            
-            silence = False
-
-            # sometimes put silence instead of a note
-            # because sometimes silence tells more than a C4
-            if random.randint(1, 100) < silence_percent:
-                silence = True
-
             # make sure the upcoming note in the bar is not the same
             # with the previous one, and also make sure we don't jump
             # between high and low notes too aggressively
@@ -160,21 +140,17 @@ def generateMusic():
 
             note_last = note_current
 
-            #if not silence:
-                #print("\nNote: " + str(note_current_name))
-            #else:
-                #print("\nSilence")
-                
-            #print("Duration: " + str(duration))
+            music.append([note_current,duration])
 
-            if not silence:
-                music.append([note_current,duration])
-                #player.play_wave(synth1.generate_constant_wave(note_current, duration * 60/bpm_current))
-            else:
-                music.append([-1,duration])
-                #time.sleep(duration * 60/bpm_current)
+    return godsaid, music
 
-    return music
+app = Flask(__name__, template_folder='templates')
+app.debug = True
+app._static_folder = os.path.abspath("templates/static/")
 
-generateMusic()
-
+@app.route('/', methods=['GET'])
+def index():
+    words, song = generateMusic()
+    return render_template('layouts/index.html',
+                           words=words,
+                           song=song)
