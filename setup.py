fbcb0f5a805e66d4080f7b0bdb1dd2fca8e83ad6
arda-guler <ardaguler09@gmail.com>
2021-08-24 11:35:22 +0300

    Prepare for packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytunegen",
+    version="1.0.0",
+    author="arda-guler",
+    author_email="ardaguler09@gmail.com",
+    description="Generates randomized music tunes with Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arda-guler/pyTuneGen",
+    project_urls={
+        "Bug Tracker": "https://github.com/arda-guler/pyTuneGen/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
